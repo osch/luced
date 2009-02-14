285d534e1765a5d2c907748c97d7eb4d3705f2b8
osch <oliver at luced de>
2009-02-14 18:33:15 +0100

    improved Nullable<T>: value doesn't need to be default constructable and template spezialisations for smart pointers

--- editor/Nullable.hpp
+++ editor/Nullable.hpp
@@ -2,7 +2,7 @@
 //
 //   LucED - The Lucid Editor
 //
-//   Copyright (C) 2005-2008 Oliver Schmidt, oliver at luced dot de
+//   Copyright (C) 2005-2009 Oliver Schmidt, oliver at luced dot de
 //
 //   This program is free software; you can redistribute it and/or modify it
 //   under the terms of the GNU General Public License Version 2 as published
@@ -22,6 +22,10 @@
 #ifndef NULLABLE_HPP
 #define NULLABLE_HPP
 
+#include <new>
+
+#include "debug.hpp"
+
 namespace LucED
 {
 
@@ -32,17 +36,141 @@ template
 class Nullable
 {
 public:
-    Nullable()           : valid(false), v()  {}
-    Nullable(const V& v) : valid(true),  v(v) {}
+    Nullable()           : valid(false) {}
+    Nullable(const V& v) : valid(true)  { new(&value) V(v); }
+    
+    ~Nullable() {
+        if (valid) {
+            ((V*)&(value))->~V();
+        }
+    }
+    
+    Nullable(const Nullable& rhs) : valid (rhs.valid) {
+        ASSERT(this != &rhs);
+        if (valid) {
+            new(&value) V(*(const V*)(&rhs.value));
+        }
+    }
+    
+    template<class V2
+            >
+    Nullable(const Nullable<V2>& rhs) : valid (rhs.valid) {
+        ASSERT(this != &rhs);
+        if (valid) {
+            new(&value) V(*(const V2*)(&rhs.value));
+        }
+    }
+    
+    Nullable& operator=(const Nullable& rhs) {
+        if (this != &rhs) {
+            if (valid && rhs.valid)
+            {
+                *((V*)&(value)) = *(const V*)(&rhs.value);
+            }
+            else
+            {
+                if (valid) {
+                    ((V*)&(value))->~V();
+                }
+                valid = rhs.valid;
+                if (valid) {
+                    new(&value) V(*(const V*)(&rhs.value));
+                }
+            }
+        }
+        return *this;
+    }
+    
+    template<class V2
+            >
+    Nullable& operator=(const Nullable<V2>& rhs) {
+        if (this != &rhs) {
+            if (valid && rhs.valid)
+            {
+                *((V*)&(value)) = *(const V2*)(&rhs.value);
+            }
+            else
+            {
+                if (valid) {
+                    ((V*)&(value))->~V();
+                }
+                valid = rhs.valid;
+                if (valid) {
+                    new(&value) V(*(const V2*)(&rhs.value));
+                }
+            }
+        }
+        return *this;
+    }
 
     bool     isValid() const { return valid; }
     bool     isNull()  const { return !valid; }
-    operator V()       const { return v; }
-    V        get()     const { return v; }
+    operator V()       const { ASSERT(valid); return *(const V*)(&value); }
+    V        get()     const { ASSERT(valid); return *(const V*)(&value); }
 
 private:
+    struct { char data[sizeof(V)]; } value;
     bool valid;
-    V v;
+};
+
+
+template<class T> class OwningPtr;
+template<class T> class WeakPtr;
+template<class T> class RawPtr;
+
+
+template
+<
+    class T
+>
+class Nullable< OwningPtr<T> >
+{
+public:
+    Nullable() {}
+    Nullable(const OwningPtr<T>& v) : ptr(v) {}
+
+    bool         isValid()      const { return ptr.isValid(); }
+    bool         isNull()       const { return !ptr.isValid(); }
+    operator     OwningPtr<T>() const { return ptr; }
+    OwningPtr<T> get()          const { return ptr; }
+private:
+    OwningPtr<T> ptr;
+};
+
+template
+<
+    class T
+>
+class Nullable< WeakPtr<T> >
+{
+public:
+    Nullable() {}
+    Nullable(const WeakPtr<T>& v) : ptr(v) {}
+
+    bool       isValid()      const { return ptr.isValid(); }
+    bool       isNull()       const { return !ptr.isValid(); }
+    operator   WeakPtr<T>()   const { return ptr; }
+    WeakPtr<T> get()          const { return ptr; }
+private:
+    WeakPtr<T> ptr;
+};
+
+template
+<
+    class T
+>
+class Nullable< RawPtr<T> >
+{
+public:
+    Nullable() {}
+    Nullable(const RawPtr<T>& v) : ptr(v) {}
+
+    bool      isValid()     const { return ptr.isValid(); }
+    bool      isNull()      const { return !ptr.isValid(); }
+    operator  RawPtr<T>()   const { return ptr; }
+    RawPtr<T> get()         const { return ptr; }
+private:
+    RawPtr<T> ptr;
 };
 
 } // namespace LucED
